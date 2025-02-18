44455c0f5782af7d7d74b220bf948381ac140f0f
UP240757 <up240757@alumnos.upa.edu.mx>
2025-02-18 08:55:59 -0600

    Ejercicio1.py

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,3 +1,11 @@
 age = 18 
 print (age)
 print (type(age))
+
+#Program 4
+# Write a script that prompts the user to enter base and height 
+# of the trianngle and calculate an area of this triangle
+# (area = 0.5 x b x h). 
+base = float(input ("Ingresa el valor de la base: "))
+altura = float(input("Ingresa el valor de la altura: "))
+area = print (base*altura/2)
